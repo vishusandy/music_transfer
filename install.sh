cc5e1a2173930e8429bd45f3dda45defdb9ca42b
Andrew <kaioshin2005@yahoo.com>
2023-09-07 10:38:15 -0500

    install.sh always activates venv

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ fi
 
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
-    #shellcheck disable=SC1091
-    source .venv/bin/activate
 fi
+
+#shellcheck disable=SC1091
+source .venv/bin/activate
 
 pip install --editable .
